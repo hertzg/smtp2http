a3950007705fe082833653741efc6d39ecc2ed1c
root <root@storage2.listen.ge>
2013-07-04 03:48:47 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.port = 1025
+exports.host = '127.0.0.1'
+
+exports.hosts = {
+    'atvirte.com': 'http://atvirte.com/mail.php?key=123',
+    //'example.com': 'http://192.168.0.2/handle-example.php',
+}
